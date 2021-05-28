5852012d9aadb821d37dbc847bdb9da8849174c1
harshit2001sharma@gmail.com <44293679+Harshit123Sharma@users.noreply.github.com>
2021-05-28 18:40:48 +0530

    Added SearchBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./App.css";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   render(){
   return (
-    <div>
+    <div className="ui container">
       <SearchBar />
     </div>
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,34 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = { term: "" };
+
+    onInputChange = event => {
+        this.setState({ term: event.target.value });
+    };
+
+    onFormSubmit = event => {
+        event.preventDefault();
+    };
+
     render() {
         return (
-            <div> Search </div>
+          <div className="search-bar ui inverted segment">
+            <form onSubmit={this.onFormSubmit} className="ui inverted form">
+              <div className="field">
+                <label>
+                  Video Search
+                  <i class="search icon" />
+                </label>
+                <input 
+                placeholder="Search" 
+                type="text" 
+                value={this.state.term}
+                onChange={this.onInputChange}
+                />
+              </div>
+            </form>
+          </div>
         );
     }
 }
